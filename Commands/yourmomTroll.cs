103bc8aa218d1e373925adb1deb33c1639618ad7
Claudio#7017 <72409092+ClaudioPanConQueso@users.noreply.github.com>
2021-07-17 14:13:23 -0300

    Add files via upload

---
+++ Commands/yourmomTroll.cs
@@ -0,0 +1,20 @@
+﻿using CommandSystem;
+using System;
+
+namespace SCPSL.Commands
+{
+    class yourmomTroll : ICommand
+    {
+        public string Command { get; } = "amogus";
+
+        public string[] Aliases { get; } = { };
+
+        public string Description { get; } = "idk";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            response = "sus?";
+            return false;
+        }
+    }
+}
