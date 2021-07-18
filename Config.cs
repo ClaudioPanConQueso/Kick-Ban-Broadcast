1cbe83dad68392123ef90b98afc772735c3249f2
Claudio#7017 <72409092+ClaudioPanConQueso@users.noreply.github.com>
2021-07-18 10:28:33 -0300

    Add files via upload

--- Config.cs
+++ Config.cs
@@ -8,7 +8,7 @@ namespace SCPSL
         public bool IsEnabled { get; set; } = true;
 
         [Description("Text which will appear when someone is kicked! {player} will be changed per player username, and {reason} will be changed per kick reason.")]
-        public string KickBroadcast { get; set; } = "{player} has been kicked from the server. reason: {reason}";
+        public string KickBroadcast { get; set; } = "{player} has been kicked from the server. Reason: {reason}";
 
         [Description("Text will C.A.S.S.I.E make something, (leave in blank to disable)")]
         public string cassiekick { get; set; } = "A user has been successfully terminated from the facility";
@@ -17,6 +17,6 @@ namespace SCPSL
         public string cassieban { get; set; } = "A user has been successfully terminated from the facility";
 
         [Description("Text which will appear when someone is banned! {player} will be changed per player username, and {reason} will be changed per ban reason.")]
-        public string BanBroadcast { get; set; } = "{player} has been banned from the server. reason: {reason}";
+        public string BanBroadcast { get; set; } = "{player} has been banned from the server. Reason: {reason}";
     }
 }
